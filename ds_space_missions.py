0a221da88fe3fed4929d6442b4f1c80de6d9d937
tfaiss <t.faiss@faito.de>
2020-08-25 18:41:04 +0200

    add initial files

---
+++ ds_space_missions.py
@@ -0,0 +1,18 @@
+    # -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+author:     Tobias Faiss <t.faiss@faito.de>
+date:       Aug 25th, 2020
+"""
+
+## import libraries
+import pandas as pd
+
+## load dataset
+path = "data/space_corrected.csv"
+
+missions = pd.read_csv(path)
+#print(missions)
+
+
