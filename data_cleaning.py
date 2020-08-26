883e1783c513ac143f7d7c5978f579989569e9bc
Tobias Faiss <t.faiss@faito.de>
2020-08-26 11:22:37 +0200

    distilling single values in data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -11,7 +11,53 @@ path = "data/space_corrected.csv"
 df = pd.read_csv(path)
 
 
-## data cleaning activities
-# separate location launch site and country
-# divide detail in 'rocket' and 'satellite'
+## FUNCTIONS
+
+
+
+
+
+## RENAMING COLUMNS
+
 # change column 'rocket' to 'Costs in m USD'
+df.rename(columns = {' Rocket':'Costs (Mio. USD)'}, inplace=True)
+
+
+## SEPARATE 'LOCATION' INTO 'LAUNCH SITE' AND 'COUNTRY'
+# distill launch site from location and add it to dataframe
+launch_site = df.Location.str.rsplit(',', 1)
+launch_site = launch_site.tolist()
+for i in launch_site:
+    i.pop()
+df['Site'] = launch_site
+
+
+# distill country from location and add it to dataframe
+country = df.Location.str.rsplit(',', -1)
+country = country.tolist()
+for i in country:
+    del i[:-1]
+df['Country'] = country
+
+
+## SEPARATE 'DETAILS' INTO 'ROCKET' AND 'SATELLITE'
+# distill rocket from details and add it to dataframe
+rocket = df.Detail.str.rsplit(' | ', 1)
+rocket = rocket.tolist()
+for i in rocket:
+    i.pop()  
+df['Rocket'] = rocket
+
+# distill satellite from details and add it to dataframe
+satellite = df.Detail.str.rsplit(' | ', 1)
+satellite = satellite.tolist()
+for i in satellite:
+    del i[:-1] 
+df['Satellite'] = satellite
+
+
+## DROP UNNECESSARY COLUMNS
+# drop unnamed columns, location and detail since it has been replaced by new columns
+df.drop(['Unnamed: 0', 'Unnamed: 0.1', 'Location', 'Detail'], axis=1, inplace=True)
+
+
